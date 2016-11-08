1afbd844ac82cbd86e8da390fd6b0311b5b4cbe9
cdowdy <cdowdy46@gmail.com>
2016-11-08 12:30:43 -0500

    adds initial HTTP signatures for signed URL's for thumbnails

--- src/Controller/BetterThumbsController.php
+++ src/Controller/BetterThumbsController.php
@@ -3,17 +3,21 @@
 namespace Bolt\Extension\cdowdy\betterthumbs\Controller;
 
 
-
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
-use Silex\RedirectableUrlMatcher;
+
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use League\Glide\Server;
+
+
 use League\Glide\ServerFactory;
 use League\Glide\Responses\SymfonyResponseFactory;
+use League\Glide\Signatures\SignatureFactory;
+use League\Glide\Signatures\SignatureException;
+
+
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 
@@ -56,20 +60,45 @@ class BetterThumbsController implements ControllerProviderInterface
         $adapter = new Local($app['resources']->getPath('filespath') );
         $Filesystem = new Filesystem($adapter);
 
+        $ImageDriver = $this->config['Image_Driver'];
+
+        $defaultSettings = $this->config['defaults'];
+
+
+
         $server = ServerFactory::create([
             'response' => new SymfonyResponseFactory(),
             'source' => $Filesystem,
             'cache' => $Filesystem,
 
-            'source_path_prefix' => $Filesystem,
-//            'source_path_prefix' => 'files',
+//            'source_path_prefix' => $Filesystem,
+            'source_path_prefix' => 'files',
             'cache_path_prefix' => '.cache',
 
-            'base_url' => 'img',
-            'driver' => 'gd',
+            'watermarks' => $Filesystem,
+
+            'base_url' => '/img/',
+            'driver' => $ImageDriver,
         ]);
 
-        ob_end_clean();
+        // make sure the URL is signed with our key before allowing manipulations done to the thumbnail
+        try {
+            // TODO: make sure you remove this dev sign key with a good one :) before pushing to production
+            // Set complicated sign key
+            $signkey = 'v-LK4WCdhcfcc%jt*VC2cj%nVpu+xQKvLUA%H86kRVk_4bgG8&CWM#k*b_7MUJpmTc=4GFmKFp7=K%67je-skxC5vz+r#xT?62tT?Aw%FtQ4Y3gvnwHTwqhxUh89wCa_';
+
+            // Validate HTTP signature
+            SignatureFactory::create($signkey)->validateRequest($path,  $request->query->all());
+
+
+        } catch (SignatureException $e) {
+//            throw new SignatureException( $e->getMessage() );
+            // the 401 works but maybe we should actually send bolts not found image?
+            return new Response('Operation Not Allowed', Response::HTTP_UNAUTHORIZED);
+        }
+
+        // ob_clean / ob_end_clean is needed here ¯\_(ツ)_/¯
+        ob_clean();
         return $server->getImageResponse($path, $request->query->all());
     }
 }
