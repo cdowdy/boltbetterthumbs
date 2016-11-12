f8a18479bbbe94e3d013ec08e9a3efe73a7c3a75
cdowdy <cdowdy46@gmail.com>
2016-11-12 13:23:18 -0500

    adds initial srcset handdler

---
+++ src/Handler/SrcsetHandler.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace Bolt\Extension\cdowdy\betterthumbs\Handler;
+
+use Silex\Application;
+
+class SrcsetHandler
+{
+    private $app;
+
+    /**
+     * @var array
+     */
+    protected $_extensionConfig;
+
+    /**
+     * SrcsetHandler constructor.
+     * @param array $_extensionConfig
+     */
+    public function __construct(array $_extensionConfig)
+    {
+        $this->_extensionConfig = $_extensionConfig;
+    }
+
+
+    protected function getImageOptions( $filename, $config, array $options )
+    {
+
+    }
+
+    // get all the modifications being done to the image and pass them to / create an array
+    protected function setImageModifications(array $modifications = [] )
+    {
+
+    }
+
+    public function getSizesAttrib($configName, $default = ['100vw'] )
+    {
+        $sizes = isset( $this->_extensionConfig[$configName]['sizes'])
+            ? $this->_extensionConfig[$configName]['sizes']
+            : $default;
+
+        return $sizes;
+    }
+
+    // TODO: allow a 'd' or 'D' to be passed but conver it to 'x'
+    protected function getWidthDensity($configName, $default = 'w')
+    {
+
+        $widthDensity = isset($this->_extensionConfig[$configName][ 'widthDensity' ]);
+
+        if (isset($widthDensity) && !empty($widthDensity)) {
+            $wd = strtolower($this->_extensionConfig[$configName][ 'widthDensity' ]);
+        } else {
+            $wd = $default;
+        }
+
+        return $wd;
+    }
+
+
+    public function getResolutions($configName, $defaultResolutions = [ 1, 2, 3 ])
+    {
+
+        $resolutions = isset( $this->_extensionConfig[$configName]['resolutions']) ? $this->_extensionConfig[$configName]['resolutions'] : $defaultResolutions;
+
+        return $resolutions;
+    }
+
+    public function resolutionErrors($thumb, $resolutions)
+    {
+        $resError = [];
+        $thumbCount = count($thumb);
+        $resCount = count($resolutions);
+        // if the resolutions are more than the thumbnails remove the resolutions to match the thumbnail array
+        if ($resCount > $thumbCount) {
+
+            $newResArray = array_slice($resolutions, 0, $thumbCount);
+            $resError = array_combine($thumb, $newResArray);
+        }
+        // if the resolution count is smaller than the number of thumbnails remove the number of thumbnails
+        // to match the $resCount Array
+        if ($resCount < $thumbCount) {
+            $newThumbArray = array_slice($thumb, 0, $resCount);
+            $resError = array_combine($newThumbArray, $resolutions);
+        }
+        if ($resCount === $thumbCount ) {
+            $resError = array_combine( $thumb, $resolutions);
+        }
+        return $resError;
+    }
+
+}
