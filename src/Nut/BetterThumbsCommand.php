411a42f8a22559cdb966c9b3208e510e2e44e162
cdowdy <cdowdy46@gmail.com>
2016-12-07 12:23:32 -0500

    adds placeholders for symonfy style to style console messages

--- src/Nut/BetterThumbsCommand.php
+++ src/Nut/BetterThumbsCommand.php
@@ -3,28 +3,23 @@
 
 namespace Bolt\Extension\cdowdy\betterthumbs\Nut;
 
-//use Symfony\Component\Console\Command\Command;
-//use Symfony\Component\Console\Input\InputArgument;
-//use Symfony\Component\Console\Input\InputInterface;
-//use Symfony\Component\Console\Input\InputOption;
-//use Symfony\Component\Console\Output\OutputInterface;
-//use Bolt\Filesystem\Iterator\RecursiveDirectoryIterator;
 use Silex\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+//use Symfony\Component\Console\Style\SymfonyStyle;
 
 //use RecursiveDirectoryIterator;
 //use RecursiveIteratorIterator;
 //use SplFileInfo;
 
-use League\Glide\ServerFactory;
+//use League\Glide\ServerFactory;
 
 
 
-use League\Flysystem\Adapter\Local;
-use League\Flysystem\Filesystem;
+//use League\Flysystem\Adapter\Local;
+//use League\Flysystem\Filesystem;
 
 
 class BetterThumbsCommand extends Command
@@ -48,21 +43,24 @@ class BetterThumbsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+//        $io = new SymfonyStyle($input, $output);
         $type = $input->getArgument('type');
 //
         if ($type == 'all') {
 //            $this->doDeleteAll();
             $this->doDelete();
             $output->writeln([
-                'Removing all Images From The BetterThumbs Cache',
-                '======================================'
+                '<info>Removing all Images From The BetterThumbs Cache</info>',
+                '<info>======================================</info>'
             ]);
+//            $io->success('all Images Have Been Removed From BetterThumbs Cache');
         } else {
 //            $this->doDeleteAll($input->getArgument('type'));
             $this->doDelete($input->getArgument('type'));
             $output->writeln([
-                'removing ' . $input->getArgument('type') . ' from the BetterThumbs cache'
+                '<info>removing ' . $input->getArgument('type') . ' from the BetterThumbs cache</info>'
             ]);
+//            $io->success($input->getArgument('type') . 'Has Been Removed From BetterThumbs Cache');
         }
 
 //        $output->writeln($text);
@@ -83,44 +81,4 @@ class BetterThumbsCommand extends Command
             }
         }
     }
-
-//    protected function deleteCachedImages($dir)
-//    {
-//        if (false === file_exists($dir)) {
-//            return false;
-//        }
-//
-//        /** @var SplFileInfo[] $files */
-//        $files = new RecursiveIteratorIterator(
-//            new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
-//            RecursiveIteratorIterator::CHILD_FIRST
-//        );
-//        foreach ($files as $fileinfo) {
-//            if ($fileinfo->isDir()) {
-//                if (false === rmdir($fileinfo->getRealPath())) {
-//                    return false;
-//                }
-//            } else {
-//                if (false === unlink($fileinfo->getRealPath())) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return rmdir($dir);
-//    }
-//
-//    protected function doDeleteAll($default = NULL )
-//    {
-//        $filespath = $this->app['resources']->getPath('filespath') . '/.cache';
-//        $allFiles = array_diff(scandir($filespath), array('.', '..'));
-//
-//       if( $default ) {
-//           $this->deleteCachedImages($filespath . '/' . $default);
-//       } else {
-//           foreach ($allFiles as $file ) {
-//               $this->deleteCachedImages($filespath . '/' . $file );
-//           }
-//       }
-//    }
-
 }
