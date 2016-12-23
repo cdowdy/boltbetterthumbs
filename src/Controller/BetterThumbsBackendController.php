1ff863ed2c4b498e7880ab918d980eec788f1343
cdowdy <cdowdy46@gmail.com>
2016-12-23 10:36:32 -0500

    fixes #8 only allows logged in users to backend pages

--- src/Controller/BetterThumbsBackendController.php
+++ src/Controller/BetterThumbsBackendController.php
@@ -7,6 +7,9 @@ use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 use Bolt\Extension\cdowdy\betterthumbs\Helpers;
 use Bolt\Extension\cdowdy\betterthumbs\Helpers\ConfigHelper;
 
@@ -41,7 +44,10 @@ class BetterThumbsBackendController implements ControllerProviderInterface
     }
 
 
-
+    /**
+     * @param Application $app
+     * @return ControllerCollection
+     */
     public function connect(Application $app)
     {
         /** @var ControllerCollection $ctr */
@@ -59,15 +65,44 @@ class BetterThumbsBackendController implements ControllerProviderInterface
         $ctr->post('/files/delete/all', [$this, 'deleteAll'])
             ->bind('betterthumbs_delete_all');
 
+        // make sure the user is logged in and allowed to view the backend pages before rendering
+        $ctr->before([$this, 'before']);
+
 
         return $ctr;
     }
 
+
+    /**
+     * @param Request $request
+     * @param Application $app
+     * @return null|RedirectResponse
+     */
+    public function before(Request $request, Application $app)
+    {
+        if (!$app['users']->isAllowed('dashboard')) {
+            /** @var UrlGeneratorInterface $generator */
+            $generator = $app['url_generator'];
+            return new RedirectResponse($generator->generate('dashboard'), Response::HTTP_SEE_OTHER);
+        }
+        return null;
+    }
+
+
+    /**
+     * @param Application $app
+     * @return mixed
+     */
     public function bthumbsDocs(Application $app)
     {
         return $app['twig']->render('betterthumbs.docs.html.twig');
     }
 
+
+    /**
+     * @param Application $app
+     * @return mixed
+     */
     public function bthumbsFiles(Application $app)
     {
         $configHelper = $configHelper = new ConfigHelper($this->config);
@@ -96,6 +131,12 @@ class BetterThumbsBackendController implements ControllerProviderInterface
         return $app['twig']->render('betterthumbs.files.html.twig', $context);
     }
 
+
+    /**
+     * @param Application $app
+     * @param Request $request
+     * @return mixed
+     */
     public function deleteSingle(Application $app, Request $request)
     {
         $betterthumbs = $app['betterthumbs'];
@@ -104,6 +145,12 @@ class BetterThumbsBackendController implements ControllerProviderInterface
 
     }
 
+
+    /**
+     * @param Application $app
+     * @param Request $request
+     * @return array
+     */
     public function deleteAll(Application $app, Request $request)
     {
         $betterthumbs = $app['betterthumbs'];
@@ -115,7 +162,5 @@ class BetterThumbsBackendController implements ControllerProviderInterface
         }
 
         return $removed;
-
-
     }
 }
