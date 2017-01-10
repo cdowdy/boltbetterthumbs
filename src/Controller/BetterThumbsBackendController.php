936eb90ca4f0eba8e5be52a20378e9264803891a
cdowdy <cdowdy46@gmail.com>
2017-01-10 14:58:09 -0500

    adds make image method to pre generate images for #7

--- src/Controller/BetterThumbsBackendController.php
+++ src/Controller/BetterThumbsBackendController.php
@@ -10,7 +10,7 @@ use Silex\ControllerProviderInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-use Bolt\Extension\cdowdy\betterthumbs\Helpers;
+
 use Bolt\Extension\cdowdy\betterthumbs\Helpers\ConfigHelper;
 
 
@@ -18,12 +18,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-use League\Glide\Urls\UrlBuilderFactory;
+
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
-use League\Flysystem\Plugin\ListPaths;
-use League\Flysystem\Memory\MemoryAdapter;
-
 
 
 class BetterThumbsBackendController implements ControllerProviderInterface
@@ -191,6 +188,10 @@ class BetterThumbsBackendController implements ControllerProviderInterface
         return $removed;
     }
 
+    /**
+     * @param Application $app
+     * @return mixed
+     */
     public function primeCache(Application $app)
     {
         $adapter = new Local($app['resources']->getPath('filespath') );
@@ -243,5 +244,60 @@ class BetterThumbsBackendController implements ControllerProviderInterface
 
         return $app['twig']->render('betterthumbs.prime.html.twig', $context);
     }
+
+    /**
+     * @param Application $app
+     * @param $image
+     * @param $modifications
+     * @return mixed
+     */
+    public function primeImage(Application $app, $image, $modifications)
+    {
+        $betterthumbs = $app['betterthumbs'];
+        $betterthumbs->setCacheWithFileExtensions(true);
+
+        return $betterthumbs->makeImage( $image, $modifications);
+    }
+
+    /**
+     * @param Application $app
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function doPrime(Application $app, Request $request)
+    {
+        $modType = $request->get('modType');
+        $configName = $request->get('configName');
+        $image = $request->get('image');
+
+        $configHelper = new ConfigHelper($this->config);
+        $presetMods = $configHelper->setPresets();
+
+
+        $primed = [];
+
+        switch ($modType) {
+
+            case 'presets':
+                foreach ($presetMods as $preset) {
+                    $primed[] .= $this->primeImage($app, $image, $preset);
+                }
+                break;
+            case 'config':
+                $configMods = $this->config[$configName]['modifications'];
+                foreach ($configMods as $parameters) {
+                    $primed[] .= $this->primeImage($app, $image, $parameters);
+                }
+                break;
+            case 'single':
+                $singleParams = $this->config[$configName];
+
+                $primed[] = $this->primeImage($app, $image, $singleParams);
+                break;
+        }
+
+        return new JsonResponse($primed);
+    }
+
 
 }
